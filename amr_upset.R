3f7512ee9cda605028ee2b41c1387de9cf3b989c
Antonio <antoniomenendezperez408@gmail.com>
2026-01-19 11:16:47 +0100

    Add files via upload
    Código modificado de la función amr_upset del paquete AMRgen. La parte modificada está indicada en español.

---
+++ amr_upset.R
@@ -0,0 +1,353 @@
+amr_upset <- function(binary_matrix, min_set_size = 2, order = "",plot_set_size = FALSE, plot_category = TRUE,      print_category_counts = FALSE, print_set_size = FALSE,                  boxplot_colour = "grey", assay = "mic") {
+  
+  if (sum(!is.na(binary_matrix$pheno)) == 0) {
+    if (sum(!is.na(binary_matrix$ecoff)) > 0) {
+      binary_matrix <- binary_matrix %>% mutate(pheno = ecoff)
+      cat(" Warning: no values in pheno column, colouring upset plot by ecoff column\n")
+    } else {
+      stop(" Failed to make upset plot as no values in field pheno or ecoff")
+    }
+    colour_label <- "ECOFF\ncategory"
+  } else {
+    colour_label <- "Resistance\ncategory"
+  }
+  
+  # gene names
+  genes <- binary_matrix %>%
+    select(-any_of(c("id", "pheno", "ecoff", "R", "NWT", "mic", "disk"))) %>%
+    colnames()
+  
+  # check with have the expected assay column, with data
+  if (!(assay %in% colnames(binary_matrix))) {
+    stop(paste("input", deparse(substitute(binary_matrix)), "must have a column labelled ", assay))
+  }
+  
+  data_rows <- binary_matrix %>%
+    filter(!is.na(.data[[assay]])) %>%
+    nrow()
+  
+  if (data_rows == 0) {
+    stop(paste("input", deparse(substitute(binary_matrix)), "has no non-NA values in column ", assay))
+  }
+  
+  # Add in a combination column and filter to samples with the required assay data (MIC or disk) only
+  binary_matrix_wide <- binary_matrix %>%
+    filter(!is.na(get(assay))) %>%
+    unite("combination_id", genes[1]:genes[length(genes)], remove = FALSE) # add in combinations
+  
+  # Make matrix longer
+  binary_matrix <- binary_matrix_wide %>%
+    pivot_longer(cols = genes[1]:genes[length(genes)], names_to = "genes") %>%
+    mutate(genes = gsub("\\.\\.", ":", genes)) %>%
+    mutate(genes = gsub("`", "", genes))
+  
+  ### Counts per combination, for bar plot - X axis = combination. Y axis = number of strains ###
+  # This first to filter on combinations with enough data
+  combination_freq <- binary_matrix_wide %>%
+    group_by(combination_id) %>%
+    summarise(n = n()) %>%
+    mutate(perc = 100 * n / sum(n)) %>% # count number with each combination
+    filter(n >= min_set_size) # count filter
+  
+  if (nrow(combination_freq) == 0) {
+    stop(paste("No marker combinations pass the minimum frequency:", min_set_size))
+  }
+  # which have enough strains / data
+  comb_enough_strains <- combination_freq %>% pull(combination_id)
+  
+  ### data for assay plot - dot plot. X axis = combination. Y axis = MIC or disk zone #####
+  
+  # Modificación principal
+  
+  assay_plot <- binary_matrix_wide %>%  # Formato ancho
+    # con binary_matrix_wide cuenta cada columna como una variable (como tenemos guardados los datos).
+    
+    #original
+    #   assay_plot <- binary_matrix %>%
+    filter(combination_id %in% comb_enough_strains) %>%
+    group_by(combination_id, get(assay), pheno)  %>% 
+    summarise(n = n()) # count how many at each assay value, keep pheno for colour
+  
+  # Fin de la modificación
+  
+  ### Gene prevalence plot (amongst strains with marker combinations exceeding the minimum set size)
+  gene.prev <- binary_matrix %>%
+    filter(combination_id %in% comb_enough_strains) %>%
+    group_by(genes) %>%
+    summarise(gene.prev = sum(value))
+  
+  ### Set order of genes for y axis in combination dot plot
+  ## (amongst strains with marker combinations exceeding the minimum set size, that will be included in plot)
+  gene.order.desc <- gene.prev %>%
+    arrange(desc(gene.prev)) %>%
+    filter(gene.prev > 0) %>%
+    pull(genes)
+  
+  # For gene prev plot
+  gene.prev <- gene.prev %>%
+    filter(genes %in% gene.order.desc) %>%
+    mutate(genes = factor(genes, levels = gene.order.desc))
+  
+  # For combination dot plot
+  binary_matrix <- binary_matrix %>%
+    filter(combination_id %in% comb_enough_strains) %>%
+    filter(genes %in% gene.order.desc) %>%
+    mutate(genes = factor(genes, levels = gene.order.desc))
+  
+  ### Point plot - X axis = combination. Y axis = genes. Lines joining genes in same strain ###
+  binary_matrix$point_size <- 2 *binary_matrix$value # want dot size to be larger than 1 => can make 2/3/4 etc
+  
+  # Only plot a line between points if more than one gene in a combination
+  # get how many in each combination
+  multi_genes_combination_id_all <- binary_matrix %>%
+    select(combination_id, genes, value, point_size) %>%
+    group_by(combination_id) %>%
+    filter(value == 1) %>%
+    mutate(u = length(unique(genes))) %>%
+    filter(genes %in% gene.order.desc) %>%
+    mutate(genes = factor(genes, levels = gene.order.desc, ordered = TRUE))
+  
+  # get only those with > 1
+  multi_genes_combination_ids <- multi_genes_combination_id_all %>%
+    filter(u > 1) %>%
+    group_by(combination_id) %>%
+    mutate(
+      min = min(genes),
+      max = max(genes)
+    ) %>%
+    ungroup()
+  
+  ### Set order of combination_id <- x axis
+  # Default = decreasing frequency
+  ordered_comb_order <- combination_freq %>%
+    arrange(desc(perc)) %>%
+    pull(combination_id)
+  assay_plot$combination_id <- factor(assay_plot$combination_id, levels = ordered_comb_order)
+  combination_freq$combination_id <- factor(combination_freq$combination_id, levels = ordered_comb_order)
+  binary_matrix$combination_id <- factor(binary_matrix$combination_id, levels = ordered_comb_order)
+  
+  # Do by # genes in combination (only want each id once)
+  if (order == "genes") {
+    ordered_comb_order <- multi_genes_combination_id_all %>%
+      arrange(u) %>%
+      filter(row_number() == 1) %>%
+      pull(combination_id)
+    assay_plot$combination_id <- factor(assay_plot$combination_id, levels = ordered_comb_order)
+    combination_freq$combination_id <- factor(combination_freq$combination_id, levels = ordered_comb_order)
+    binary_matrix$combination_id <- factor(binary_matrix$combination_id, levels = ordered_comb_order)
+  }
+  # Do by # median assay value in combination (only want each id once)
+  if (order == "value") {
+    if (assay == "mic") {
+      # use mic class median (ie ignoring range indicators <>=) for the purpose of ordering columns
+      mic_medians <- binary_matrix_wide %>%
+        group_by(combination_id) %>%
+        summarise(median = median(mic))
+      ordered_comb_order <- mic_medians %>%
+        arrange(median) %>%
+        pull(combination_id)
+    } else {
+      disk_medians <- binary_matrix_wide %>%
+        group_by(combination_id) %>%
+        summarise(median = median(as.double(disk)))
+      ordered_comb_order <- disk_medians %>%
+        arrange(-median) %>%
+        pull(combination_id)
+    }
+    assay_plot$combination_id <- factor(assay_plot$combination_id, levels = ordered_comb_order)
+    combination_freq$combination_id <- factor(combination_freq$combination_id, levels = ordered_comb_order)
+    binary_matrix$combination_id <- factor(binary_matrix$combination_id, levels = ordered_comb_order)
+  }
+  
+  ##### Plots ###
+  
+  ### assay data plot (MIC/disk)
+  
+  g1 <- ggplot(data = assay_plot, aes(x = combination_id, y = `get(assay)`)) +
+    geom_boxplot(colour = boxplot_colour) +
+    geom_point(aes(size = n, colour = pheno), show.legend = TRUE) +
+    theme_bw() +
+    scale_size_continuous("Number of\nisolates") +
+    scale_color_sir(name = colour_label) +
+    theme(
+      axis.text.x = element_blank(),
+      axis.title.x = element_blank(),
+      axis.ticks.x = element_blank()
+    )
+  
+  if (assay == "mic") {
+    g1 <- g1 +
+      scale_y_mic() +
+      ylab("MIC (mg/L)")
+  } else {
+    g1 <- g1 +
+      ylab("Disk zone (mm)")
+  }
+  
+  ### Bar plot - set size
+  g2 <- ggplot(combination_freq, aes(x = combination_id, y = n)) +
+    geom_bar(stat = "identity") +
+    theme_bw() +
+    scale_y_reverse("Set size") +
+    scale_x_discrete("group") +
+    theme(
+      axis.text.x = element_blank(),
+      axis.title.x = element_blank(),
+      axis.ticks.x = element_blank()
+    )
+  if (print_set_size) {
+    g2 <- g2 + geom_text(aes(label = n), nudge_y = -.5)
+  }
+  
+  # pheno category stacked barplot
+  category_plot <- binary_matrix %>%
+    select(id, pheno, combination_id) %>%
+    distinct() %>%
+    ggplot(aes(x = combination_id, fill = pheno)) +
+    geom_bar(stat = "count", position = "fill") +
+    scale_fill_sir() +
+    theme_light() +
+    labs(x = "", y = "Category") +
+    theme(
+      axis.text.x = element_blank(),
+      axis.title.x = element_blank(),
+      axis.ticks.x = element_blank(),
+      legend.position = "none"
+    )
+  if (print_category_counts) {
+    category_plot <- category_plot +
+      geom_text(aes(label = after_stat(count)), stat = "count", position = position_fill(vjust = .5), size = 3)
+  }
+  
+  ### Dot plot of combinations
+  
+  g3 <- binary_matrix <- binary_matrix %>%
+    mutate(binary_comb = if_else(value > 0, 1, 0)) %>%
+    ggplot(aes(x = combination_id, y = fct_rev(genes))) +
+    geom_point(aes(size = binary_comb), show.legend = FALSE) +
+    theme_bw() +
+    scale_size_continuous(range = c(-1, 2)) +
+    scale_y_discrete(name = "Marker") +
+    geom_segment(
+      data = multi_genes_combination_ids,
+      aes(
+        x = combination_id, xend = combination_id,
+        y = min, yend = max, group = combination_id
+      ),
+      color = "black"
+    ) + # add lines
+    theme(
+      axis.text.x = element_blank(),
+      axis.title.x = element_blank(),
+      axis.ticks.x = element_blank()
+    )
+  
+  ### Plot gene prev / set size
+  g4 <- gene.prev %>%
+    ggplot(aes(x = fct_rev(genes), y = gene.prev)) +
+    geom_col() +
+    theme_bw() +
+    coord_flip() +
+    scale_y_reverse("") +
+    theme(
+      axis.text.y = element_blank(),
+      axis.title.y = element_blank(),
+      axis.ticks.y = element_blank()
+    )
+  
+  # assemble plot
+  final_plot <- plot_spacer() + g1 + plot_layout(ncol = 2, widths = c(1, 4), guides = "collect")
+  if (plot_category) {
+    final_plot <- final_plot + plot_spacer() + category_plot
+  }
+  final_plot <- final_plot + g4 + g3
+  if (plot_set_size) {
+    final_plot <- final_plot + plot_spacer() + g2
+  }
+  
+  # set relative plotting heights
+  if (plot_category & plot_set_size) {
+    final_plot <- final_plot + plot_layout(heights = c(2, 1, 2, 1))
+  }
+  if (plot_category & !plot_set_size) {
+    final_plot <- final_plot + plot_layout(heights = c(2, 1, 2))
+  }
+  if (!plot_category & plot_set_size) {
+    final_plot <- final_plot + plot_layout(heights = c(2, 2, 1))
+  }
+  
+  print(final_plot)
+  
+  # summary table (ignore MIC values expressed as ranges, when calculating median/IQR)
+  if (assay == "mic") {
+    summary <- binary_matrix_wide %>%
+      group_by(combination_id) %>%
+      summarise( #note these medians are not meaningful if all expressed as ranges
+        median_ignoreRanges = median(mic, na.rm = TRUE),
+        q25_ignoreRanges = stats::quantile(mic, 0.25, na.rm = TRUE),
+        q75_ignoreRanges = stats::quantile(mic, 0.75, na.rm = TRUE),
+        n = n()
+      )
+    summary <- binary_matrix_wide %>%
+      filter(!grepl("<|>", as.character(mic))) %>%
+      group_by(combination_id) %>%
+      summarise(
+        median_excludeRangeValues = median(mic, na.rm = TRUE),
+        q25_excludeRangeValues = stats::quantile(mic, 0.25, na.rm = TRUE),
+        q75_excludeRangeValues = stats::quantile(mic, 0.75, na.rm = TRUE),
+        n_excludeRangeValues = n()
+      ) %>% left_join(summary, by="combination_id")
+  } else {
+    summary <- binary_matrix_wide %>%
+      group_by(combination_id) %>%
+      summarise(
+        median = median(as.double(as.disk(disk)), na.rm = TRUE),
+        q25 = stats::quantile(as.double(as.disk(disk)), 0.25, na.rm = TRUE),
+        q75 = stats::quantile(as.double(as.disk(disk)), 0.75, na.rm = TRUE),
+        n = n(),
+        n_exclRangeValues = sum(!grepl("<|>", as.character(mic)))
+      )
+  }
+  if ("NWT" %in% colnames(binary_matrix_wide)) {
+    summary <- binary_matrix_wide %>%
+      group_by(combination_id) %>%
+      summarise(
+        NWT.ppv = mean(NWT, na.rm = TRUE),
+        NWT = sum(NWT, na.rm = TRUE)
+      ) %>%
+      right_join(summary, by="combination_id")
+  }
+  if ("R" %in% colnames(binary_matrix_wide)) {
+    summary <- binary_matrix_wide %>%
+      group_by(combination_id) %>%
+      summarise(
+        R.ppv = mean(R, na.rm = TRUE),
+        R = sum(R, na.rm = TRUE)
+      ) %>%
+      right_join(summary, by="combination_id")
+  }
+  
+  # get names for summary
+  combination_names <- binary_matrix_wide %>%
+    select(combination_id, any_of(genes)) %>%
+    distinct()
+  
+  combination_names <- combination_names %>%
+    mutate(marker_list = apply(., 1, function(row) {
+      paste(names(combination_names)[-1][row[-1] == 1], collapse = ", ")
+    }), .after = combination_id) %>%
+    mutate(marker_count = rowSums(. == 1)) %>%
+    select(combination_id, marker_list, marker_count)
+  
+  summary <- summary %>%
+    left_join(combination_names, by = "combination_id") %>%
+    select(-combination_id) %>%
+    mutate(marker_list = if_else(is.na(marker_list), "-", marker_list)) %>%
+    mutate(marker_count = if_else(is.na(marker_count), 0, marker_count)) %>%
+    relocate(marker_list, marker_count, n) %>%
+    mutate(marker_list = gsub("\\.\\.", ":", marker_list)) %>%
+    mutate(marker_list = gsub("`", "", marker_list))
+  
+  return(list(plot = final_plot, summary = summary))
+}
+save(amr_upset,file = "amr_upset.RData")
